8a0da8db379e28d35570056503e96e5e92ed4bbe
HOONSTREAM <tmdgns7469@gmail.com>
2024-11-10 18:41:37 +0900

    -
    -

--- Assets/00_Script/Manager/Character_Manager.cs
+++ Assets/00_Script/Manager/Character_Manager.cs
@@ -7,11 +7,10 @@ public class Character_Manager
     /// <summary>
     /// 현재 배치 되어있는 영웅을 저장합니다.
     /// </summary>
-   public Dictionary<string, Character_Holder> Set_Character_Dictionary = new Dictionary<string, Character_Holder>(); 
+    public Character_Holder[] Set_Character = new Character_Holder[6];
 
-
-    public void Get_Character()
+    public void Get_Character(int value, string character_name)
     {
-
+        Set_Character[value] = Base_Manager.Data.Data_Character_Dictionary[character_name];
     }
 }
---
+++ Assets/00_Script/Other/Render_Hero.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Render_Hero : MonoBehaviour
+{
+    public ParticleSystem[] Plus_Particles;
+}
