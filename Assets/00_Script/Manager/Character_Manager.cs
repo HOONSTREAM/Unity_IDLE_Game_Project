2b2983fb8a29fc57771f142cb66499b86e8933ad
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2025-03-01 09:06:09 +0900

    25.03.01 UPDATED
    영웅 중복배치 버그 수정

--- Assets/00_Script/Manager/Character_Manager.cs
+++ Assets/00_Script/Manager/Character_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character_Manager 
@@ -9,9 +10,23 @@ public class Character_Manager
     /// </summary>
     public Character_Holder[] Set_Character = new Character_Holder[6];
 
+
     public void Get_Character(int value, string character_name)
     {
-        Set_Character[value] = Base_Manager.Data.Data_Character_Dictionary[character_name];
+        Character_Holder newChar = Base_Manager.Data.Data_Character_Dictionary[character_name];
+
+        for(int i = 0; i<Set_Character.Length; i++)
+        {
+            if (Set_Character[i]!=null && Set_Character[i].Data.Character_EN_Name == newChar.Data.Character_EN_Name)
+            {
+                Base_Canvas.instance.Get_Toast_Popup().Initialize("영웅을 선택하세요.");
+                return;
+            }
+        }
+
+        Set_Character[value] = newChar;
+        Base_Canvas.instance.Get_Toast_Popup().Initialize("영웅 배치가 완료되었습니다!");
+       
     }
 
     public void Disable_Character(int value)
