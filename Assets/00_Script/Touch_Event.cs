d53fb031766ff8d72cc8cb7227113a58e5a71426
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2025-03-08 15:31:40 +0900

    25.03.08 UPDATED
    터치이펙트 구현

---
+++ Assets/00_Script/Touch_Event.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch_Event : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera UI_camera;
+    [SerializeField]
+    private ParticleSystem Touch_Effect; 
+ 
+    private void Update()
+    {
+        Set_Touch_Effect();
+    }
+
+    private void Set_Touch_Effect()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                // 터치 위치를 UI 카메라 기준 월드 좌표로 변환
+                Vector3 touchPos = UI_camera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, UI_camera.nearClipPlane));
+
+                // 파티클 시스템 위치 이동
+                Touch_Effect.transform.position = new Vector3(touchPos.x, touchPos.y, Touch_Effect.transform.position.z);
+
+                // 파티클 시스템 재생
+                Touch_Effect.Play();
+            }
+        }
+    }
+
+}
