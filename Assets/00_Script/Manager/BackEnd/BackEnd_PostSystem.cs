b8922499aa33cb3baf269a7c6c240d4226857186
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2025-03-29 19:47:54 +0900

    -
    -

--- Assets/00_Script/Manager/BackEnd/BackEnd_PostSystem.cs
+++ Assets/00_Script/Manager/BackEnd/BackEnd_PostSystem.cs
@@ -14,6 +14,11 @@ public class BackEnd_PostSystem : MonoBehaviour
         PostListGet(PostType.Admin);
     }
 
+    public void ReCeive_Button()
+    {
+        PostReceive(PostType.Admin, 0);
+    }
+
     public void PostListGet(PostType postType)
     {
         Backend.UPost.GetPostList(postType, callback =>
@@ -98,6 +103,122 @@ public class BackEnd_PostSystem : MonoBehaviour
                 }
             }
         });
+    }
+    public void PostReceive(PostType postType, int index)
+    {
+        if(postList.Count <= 0)
+        {
+            Debug.LogWarning("받을 수 있는 우편이 존재하지 않습니다. 혹은 우편 리스트 불러오기를 먼저 호출해주세요.");
+            return;
+        }
+
+        if(index >= postList.Count)
+        {
+            Debug.LogError($"해당 우편은 존재하지 않습니다. : 요청 : index : {index} / 우편 최대 갯수 :{postList.Count}");
+            return;
+        }
+
+        Debug.Log($"{postType.ToString()}의 {postList[index].inDate} 우편 수령을 요청합니다.");
+
+
+        Backend.UPost.ReceivePostItem(postType, postList[index].inDate, callback =>
+        {
+            if (!callback.IsSuccess())
+            {
+                Debug.LogError($"{postType.ToString()}의 {postList[index].inDate} 우편 수령에 에러가 발생하였습니다. : {callback}");
+            }
+
+            Debug.Log($"{postType.ToString()}의 {postList[index].inDate} 우편 수령에 성공하였습니다. : {callback}");
+            postList.RemoveAt(index);
+
+            if (callback.GetFlattenJSON()["postItems"].Count > 0)
+            {
+                SavePostToLocal(callback.GetFlattenJSON()["postItems"]);
+
+                _=Base_Manager.BACKEND.WriteData();
+            }
+
+            else
+            {
+                Debug.LogWarning("수령 가능한 우편이 존재하지 않습니다.");
+            }
+        });
+
+
+    }
+    public void PostReceiveAll(PostType postType)
+    {
+        if(postList.Count <= 0)
+        {
+            Debug.LogWarning("받을 수 있는 우편이 존재하지 않습니다. 혹은 우편 리스트 불러오기를 먼저 호출해주세요.");
+            return;
+        }
+
+        Debug.Log($"{postType.ToString()} 우편 전체 수령을 요청합니다.");
+
+        Backend.UPost.ReceivePostItemAll(postType, callback =>
+        {
+            if (!callback.IsSuccess())
+            {
+                Debug.LogError($"{postType.ToString()} 우편 전체 수령 중 에러가 발생했습니다. : {callback}");
+                return;
+            }
+
+            Debug.Log($"우편 전체 수령에 성공하였습니다. : {callback}");
+
+            postList.Clear(); // 모든 우편을 수령하여서, postList는 초기화한다.
+
+            foreach(LitJson.JsonData postItemsJson in callback.GetFlattenJSON()["postItems"])
+            {
+                SavePostToLocal(postItemsJson);
+            }
+
+            _ = Base_Manager.BACKEND.WriteData();
+
+        });
+        
+    }
+    public void SavePostToLocal(LitJson.JsonData item)
+    {
+        try
+        {
+            foreach(LitJson.JsonData itemJson in item)
+            {
+                // 차트 파일 이름(*.xlsx) 와 Backend Console 에 등록한 차트 이름
+                string chartFileName = itemJson["item"]["chartFileName"].ToString();
+                string chartName = itemJson["chartName"].ToString();
+
+                //GoodsChart.xlsx 에 등록한 첫번째 행 이름
+
+                int ItemId = int.Parse(itemJson["item"]["ItemId"].ToString());
+                string itemName = itemJson["item"]["ItemName"].ToString();
+                string itemInfo = itemJson["item"]["ItemInfo"].ToString();
+
+                // 우편 발송 할 때 , 작성하는 아이템 수량
+
+                int itemCount = int.Parse(itemJson["itemCount"].ToString());
+
+                // 우편으로 받은 재화를 게임 내 데이터에 적용
+                // 여러 재화가 생기면, 여기를 업데이트 해야합니다!
+                if (chartName.Equals(Base_Manager.instance.GOODS_CHART_NAME))
+                {
+                    if (itemName.Equals("Dia"))
+                    {
+                        Data_Manager.Main_Players_Data.DiaMond += itemCount;
+                    }
+                }
+
+                Debug.Log($"{chartName} - {chartFileName}");
+                Debug.Log($"{ItemId} {itemName} : {itemInfo}, 획득 수량 : {itemCount}");
+                Debug.Log($"아이템을 수령했습니다. : {itemName} - {itemCount}개");
+
+            }
+        }
+
+        catch(System.Exception e)
+        {
+            Debug.LogError(e);
+        }
     }
 
 
