31f0424d6cd26af453c6a1db5bbc20ed55f02edc
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2025-05-02 13:24:09 +0900

    -
    -

--- Assets/00_Script/Skill/Character/temp_Skill.cs
+++ Assets/00_Script/Skill/Character/temp_Skill.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PalaDin_Skill : Skill_Base
+public class temp_Skill : Skill_Base
 {
     [SerializeField]
     private const float PalaDin_SKILL_DURATION_TIME = 10.0f;
     private const string PALADIN_NAME = "PalaDin";
-    
+    List<double> originalAtkList = new List<double>();
     public override void Set_Skill()
     {
         gameObject.GetComponent<Speech_Character>().Init();
@@ -21,10 +21,24 @@ public class PalaDin_Skill : Skill_Base
         Skill_Effect.gameObject.SetActive(true);
         Base_Manager.SOUND.Play(Sound.BGS, PALADIN_NAME);
         double temp = Base_Manager.Player.Get_HP(Rarity.Common, Base_Manager.Data.character_Holder[PALADIN_NAME]);
+
+
+
+        foreach(var players in players)
+        {
+            originalAtkList.Add(players.ATK);
+            players.ATK *= 100.0f;
+        }
+
         gameObject.GetComponent<Player>().HP *= 2.0d;
         yield return new WaitForSeconds(PalaDin_SKILL_DURATION_TIME);       
         Skill_Effect.gameObject.SetActive(false);
         gameObject.GetComponent<Player>().HP = temp;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].ATK = originalAtkList[i];
+        }
 
         ReturnSkill();
     }
